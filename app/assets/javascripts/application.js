09872584c9f87faf37ced252cf4581ccaa73f0ec
Nbeanavides23 <franco.noly@gmail.com>
2018-10-09 14:18:43 -0400

    add a new gemfile and semantic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+document.addEventListener("turbolinks:load", function(){
+    $('#toggle').click(function(){
+        $('.ui.sidebar').sidebar('toggle');
+      });
+})
